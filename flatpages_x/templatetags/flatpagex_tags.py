8db9a9f223369e32fb2187c9d927e4221fcf9956
Christopher Clarke <cclarke@chrisdev.com>
2012-02-20 07:33:15 -0400

    I've removed all the templatetags except the get_images as the other templategtags (meta descriptions and keywords templatestags) are unecessary after recorganizing the FlatPageMeta model.

--- flatpages_x/templatetags/flatpagex_tags.py
+++ flatpages_x/templatetags/flatpagex_tags.py
@@ -4,61 +4,6 @@ from flatpages_x.models import FlatPageMeta,FlatPageImage
 
 register = template.Library()
 
-@register.tag('meta_keywords')
-def do_meta_keywords(parser, token):
-    """
-    Get the meta keywords associated with the flatpage and 
-    stores them in a context_variable
-    
-    Usage:
-    Get the keywords associated with the flat page
-        {% meta_description as context_var  %}    
-    """
-    bits= token.split_contents()
-    if len(bits) != 3:
-        raise template.TemplateSyntaxError, "this tag takes 2 arguments" 
-       
-    return KeywordsNode(bits[-1])
-
-class KeywordsNode(template.Node):
-    def __init__(self, context_var):
-        
-        self.context_var=context_var
-        
-    def render(self, context):
-        fp=context.get('flatpage')
-        try:
-            kw=FlatPageMeta.objects.filter(flatpage =fp)[0].keywords
-        except IndexError:
-            kw=''
-        context[self.context_var]= kw
-        return ''
-
-@register.tag('meta_description')
-def do_meta_description(parser, token):
-    """
-    Get the meta description tag associated with the flatpage a
-    and stores it a context variable
-    {% meta_description as [contex_var] %}
-    """
-    bits= token.split_contents()
-    if len(bits) != 3:
-        raise template.TemplateSyntaxError, "this tag takes 2 arguments"
-    return MetaDescriptionNode(bits[-1])
-
-class MetaDescriptionNode(template.Node):
-    def __init__(self,var_name):
-        self.var_name=var_name
-    def render(self, context):
-        fp=context.get('flatpage')
-        try:
-            des=FlatPageMeta.objects.filter(
-                      flatpage = fp)[0].description
-        except IndexError:
-            des=''   
-        context[self.var_name]= des
-        return ' '
-
 @register.tag
 def get_images(parser,token):
     """
@@ -94,18 +39,13 @@ class ImagesNode(template.Node):
         self.slug_list=slug_list
         self.context_var=context_var
     def render(self,context):
-        print self.condition,self.slug_list,self.context_var
         fp=context.get('flatpage')
         if self.condition is None:
-            print 'here'
-            image_list=FlatPageImage.objects.filter(flatpage=fp)
+            image_list=fp.objects.all()
         elif self.condition=='exclude':
-            print 'there'
-            image_list=FlatPageImage.objects.filter(flatpage=fp).exclude(slug__in=self.slug_list)
+            image_list=fp.images.all().exclude(slug__in=self.slug_list)
         else:
-            print 'there'
-            image_list=FlatPageImage.objects.filter(flatpage=fp,slug__in=self.slug_list)
-            print image_list
+            image_list=fp.images.filter(slug__in=self.slug_list)
         
         context[self.context_var]=image_list
         return ''
